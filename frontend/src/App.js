7b1270281ab696fc78d902e63c82691b0df9badf
Bozo0 <serban.n.stanescu@gmail.com>
2024-04-10 22:23:28 +0300

    1

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+// //LiMy59fk8WxSwn9Ck1mnSE8wQ08Y0CBK
+
+// // https://api.tomtom.com/map/1/tile/basic/main/0/0/0.png?
+// // view=Unified&key=LiMy59fk8WxSwn9Ck1mnSE8wQ08Y0CBK
+
+// //the init for the maps
+// TomTom.setProductInfo('YourAppName', '1.0');
+// var map = tt.map({
+//     key: 'LiMy59fk8WxSwn9Ck1mnSE8wQ08Y0CBK',
+//     container: 'map', // DOM element ID
+//     style: 'tomtom://vector/1/basic-main'
+// });
